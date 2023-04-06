d1ded41a3219ac725deb6ccbc663356eb3fc2bbd
Giancarlo Massoni <massonicarlo55@gmail.com>
2023-04-06 20:13:12 +0000

    Added home component

--- wtjbjj/src/App.tsx
+++ wtjbjj/src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
-
 import NavBar from './navbar';
+import Home from './home';
 function App() {
+
+
+
   return (
 
       <NavBar/>
