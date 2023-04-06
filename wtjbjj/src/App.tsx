e1d4a263e5d363e8b3814b69fed560f0afc1c9f9
Giancarlo Massoni <massonicarlo55@gmail.com>
2023-04-06 02:08:35 +0000

    Added react bootstrap

--- wtjbjj/src/App.tsx
+++ wtjbjj/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import NavBar from './navbar';
 function App() {
   return (
+    <div>
+      <NavBar/>
         <p>Hello</p>
+        </div>
   );
 }
 
--- wtjbjj/src/navbar.tsx
+++ wtjbjj/src/navbar.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-
 function NavBar() {
   return(
     <div className="container">
-
+      <div className="row">
+        <div className="col">
+          <p>Hello</p>
+        </div>
+        <div className="col">
+          hello
+        </div>
+      </div>
     </div>
   )
 }
