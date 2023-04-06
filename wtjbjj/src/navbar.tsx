874a7b971487144f0c5f598a6544cef6226d0638
Giancarlo Massoni <massonicarlo55@gmail.com>
2023-04-06 04:45:46 +0000

    Added green to navbar

--- wtjbjj/src/navbar.tsx
+++ wtjbjj/src/navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function NavBar() {
   return(
-      <nav className="navbar navbar-expand-lg bg-light">
+      <nav className="navbar navbar-expand-lg bg-success">
         <div className="container">
           <img src="./logo.jpeg" className="logo navbar-brand" alt="logo"></img>
 
@@ -15,25 +15,25 @@ function NavBar() {
             <span className="navbar-toggler-icon justify-content-center"></span>
           </button>
 
-          <div className="collapse navbar-collapse justify-content-center" id="navmenu">
-            <ul className="navbar-nav">
-              <li className="nav-item p-4">
-                <a href="#home" className="nav-link">HOME</a>
+          <div className="collapse navbar-collapse justify-content-center px-4" id="navmenu">
+            <ul className="navbar-nav text-white">
+              <li className="nav-item px-4 ">
+              <a href="#home" className="nav-link text-white">HOME</a>
               </li>
-              <li className="nav-item p-4">
-                <a href="#schedule" className="nav-link">SCHEDULE</a>
+              <li className="nav-item px-4">
+              <a href="#schedule" className="nav-link text-white">SCHEDULE</a>
               </li>
-              <li className="nav-item p-4">
-                <a href="#coaches" className="nav-link">COACHES</a>
+              <li className="nav-item px-4">
+              <a href="#coaches" className="nav-link text-white">COACHES</a>
               </li>
-              <li className="nav-item p-4">
-               <a href="#pricing" className="nav-link">PRICING</a>
+              <li className="nav-item px-4">
+              <a href="#pricing" className="nav-link text-white">PRICING</a>
               </li>
-              <li className="nav-item p-4">
-                < a href="#blog" className="nav-link">BLOG</a>
+              <li className="nav-item px-4">
+              < a href="#blog" className="nav-link text-white">BLOG</a>
               </li>
             </ul>
-            <button type="button" className="btn btn-success justify-content-end">START NOW</button>
+            <button type="button" className="btn btn-light justify-content-end">START NOW</button>
           </div>
         </div>
 
