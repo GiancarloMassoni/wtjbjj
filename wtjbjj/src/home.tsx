19b735408d6b2fa3c1b68adbd6821392160071b4
Giancarlo Massoni <massonicarlo55@gmail.com>
2023-04-12 03:28:13 +0000

    added dev container

--- wtjbjj/src/home.tsx
+++ wtjbjj/src/home.tsx
@@ -2,6 +2,15 @@ import React from "react";
 
 function Home() {
 
+  return(
+    <div className="container hero-img">
+      <div className="row">
+        <div className="col">
+          <img src="./bjjimg1.jpeg" alt="" className="mx-auto d-block"/>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home;
