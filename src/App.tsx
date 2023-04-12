89071675c995ed0551415de4dad780b64ce97bd4
Giancarlo Massoni <massonicarlo55@gmail.com>
2023-04-12 03:46:34 +0000

    Fixed directories

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+import NavBar from './navbar';
+import Home from './home';
+function App() {
+
+
+
+  return (
+    <>
+      <NavBar/>
+      <Home />
+      </>
+  );
+}
+
+export default App;
---
+++ src/home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+
+  return(
+    <div className="container hero-img">
+      <div className="row">
+        <div className="col">
+          <img src="./bjjimg1.jpeg" alt="" className="mx-auto d-block"/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
